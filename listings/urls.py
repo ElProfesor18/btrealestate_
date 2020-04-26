e6f717a249b543053f10666d8e34c577fcb32b08
Jay Sabale <jay.sable311@gmail.com>
2020-04-27 02:18:41 +0530

    Pages, Listings, Realtors App Created!
    Respective bootstrap was added using some static files and html.
    
    All the webpages were linked in the framework!

---
+++ listings/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+# from django.http import 
+
+urlpatterns = [
+    path('', views.index, name='listings'),
+    path('<int:listing_ind>', views.listing, name='listing'),
+    path('search/', views.search, name='search'),
+    
+]
+ 
---
+++ listings/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'listings/listings.html', {})
+
+def listing(request, id):
+    return render(request, 'listings/listings.html', {})
+
+def search(request):
+    return render(request, 'listings/listings.html', {})
