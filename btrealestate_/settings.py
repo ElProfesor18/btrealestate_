17da16a8e22fb0791f1cf54147c72a8ed747b55f
Jay Sabale <jay.sable311@gmail.com>
2020-04-27 16:47:55 +0530

    Listing and Realtor models deployed.
    Admin Area : Branding completed!

--- btrealestate_/settings.py
+++ btrealestate_/settings.py
@@ -113,3 +113,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btrealestate_/static'),
 ]
+
+# MEDIA Folders
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
