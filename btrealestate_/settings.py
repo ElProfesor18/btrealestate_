19f2f310a960e8ad3a5c0c0010f529108b9635ee
Jay Sabale <jay.sable311@gmail.com>
2020-04-29 20:45:54 +0530

    Accounts application up and working
    Worked on: Login, Register, Logout, Message Alerts

--- btrealestate_/settings.py
+++ btrealestate_/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
@@ -119,3 +120,9 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
